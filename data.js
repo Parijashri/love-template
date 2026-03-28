6f26554eac98dc6cf9aac0ae8540e2237b3685a5
Parijashri <parijashri@gmail.com>
2026-03-28 20:41:25 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,38 @@
+window.APP_DATA = {
+  name: "Neelaakshi",
+  nickname: "TimTim 💖",
+
+  letter: `hiee babyyyy gurllll <3 
+I honestly crush over u every single day...`,
+
+  profilePic: "baby.jpeg",
+  video: "rimi.mp4",
+
+  images: [
+    { src: "PIC1.jpeg", caption: "The origin of the chaos. 🍼" },
+    { src: "PIC2.jpeg", caption: "Born iconic." },
+    { src: "PIC3.jpeg", caption: "My favorite view." },
+    { src: "PIC4.jpeg", caption: "Too pretty fr." },
+    { src: "PIC5.jpeg", caption: "Main character energy." },
+    { src: "PIC6.jpeg", caption: "11/10 always." }
+  ],
+
+  reasons: [
+    "How you still care too much",
+    "The way you wake up",
+    "Your ugly cry",
+    "You’re not perfect (and I love that)",
+    "Your shy side",
+    "The hidden version of you",
+    "Your eyes",
+    "Your energy",
+    "Your chaos",
+    "Your kindness",
+    "Your voice",
+    "Your vibe",
+    "Our connection",
+    "Your growth",
+    "Your bravery",
+    "Just YOU"
+  ]
+};
